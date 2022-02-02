40e0efb20cecc62d8d3eacde0bcb13fcf95b7a4a
Juan Carlos Trimino <juancarlos@trimino.com>
2022-02-02 15:49:28 -0600

    Work with StatefulSet for mongodb.

---
+++ mongodb/scripts/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Note: If the script file was created on Windows, ensure the Linux EOL LF is used; otherwise, the
+#       Windows EOL CRLF is used, and when the script is run on Linux, the shell will return an error.
+#       To ensure the Linux EOL is used, open the script file with Notepad++ and then do the following:
+#       View -> Show Symbol -> Show End of LIne
+#       Edit -> EOL Conversion -> Unix (LF)
+#
+
+echo "* Advertised Hostname: $MONGODB_ADVERTISED_HOSTNAME"
+
+if [[ "$POD_NAME" = "mem-mongodb-0" ]]; then
+  echo "* Pod name ($POD_NAME) matches initial primary pod name; configuring node as primary."
+  export MONGODB_REPLICA_SET_MODE="primary"
+else
+  echo "* Pod name ($POD_NAME) does not match initial primary pod name; configuring node as secondary."
+  export MONGODB_REPLICA_SET_MODE="secondary"
+  export MONGODB_INITIAL_PRIMARY_PORT_NUMBER="$MONGODB_PORT_NUMBER"
+  # export MONGODB_INITIAL_PRIMARY_ROOT_PASWORD="$MONGODB_ROOT_PASSWORD"
+fi
+
+exec /usr/local/bin/docker-entrypoint.sh mongod
+# exit 0
